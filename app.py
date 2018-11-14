59a9de8afee8bb53e1493106f0e6b90827b08e1c
bergpb <lindembergdepaulo@gmail.com>
2018-11-14 01:17:13 -0300

    inseção de imagens, opção deletar adicionada

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
 from model import User, Game, db
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# get absolute path from current file and concat
+app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 
 app.secret_key = 'teste'
 
@@ -13,28 +16,27 @@ def login():
     next = request.args.get('next')
     return render_template('login.html', tab_name="Login",
                             next=next)
-    
-    
-@app.route('/logout', methods=['GET'])
-def logout():
-    session['user_logged'] = None
-    flash('Please login again.', 'red')
-    # in url_for pass a name of method to redirect into your route
-    return redirect(url_for('login'))
-    
-    
-@app.route('/auth', methods=['POST'])
+                            
+                            
+@app.route('/auth', methods=['POST',])
 def auth():
-    if 'senha' == request.form['password']:
-        session['user_logged'] = request.form['username']
-        # pass a class inside a flash message
-        flash(request.form['username'] + ' logged.', 'green')
-        next_page = request.form['next']
-        return redirect(next_page)
+    usuario = User.query.filter_by(username = request.form['username']).first()
+    print(usuario)
+    if usuario:
+        if usuario.password == request.form['password']:
+            # save username in session
+            session['user_logged'] = request.form['username']
+            # pass a class inside a flash message
+            flash(request.form['username'] + ' logged.', 'green')
+            next_page = request.form['next']
+            return redirect(next_page)
+        else:
+            flash('Password invalid', 'red')
+            return redirect(url_for('login'))
     else:
-        flash('Username or password invalid', 'red')
+        flash('Username invalid', 'red')
         return redirect(url_for('login'))
-        
+
 
 @app.route('/')
 def index():
@@ -43,7 +45,7 @@ def index():
                             tab_name="Listing Games", games=lista)
 
 
-@app.route('/new')
+@app.route('/new', methods=['GET',])
 def new():
     if 'user_logged' not in session or session['user_logged'] == None:
         flash('Please login.', 'red')
@@ -53,8 +55,8 @@ def new():
         
         return render_template('new_game.html', title='New Game',
                                 tab_name="New Game")
-    
-    
+                                
+
 @app.route('/create', methods=['POST',])
 def create():
     name = request.form['name']
@@ -63,7 +65,59 @@ def create():
     game = Game(name=name, category=category, console=console)
     db.session.add(game)
     db.session.commit()
+    # uploading image to server
+    image = request.files['image']
+    load_path = app.config['UPLOAD_PATH']
+    image.save('{}/capa_{}.jpg'.format(load_path, game.id))
     return redirect(url_for('index'))
+                                
+
+@app.route('/edit/<int:game_id>', methods=['GET',])
+def edit(game_id):
+    if 'user_logged' not in session or session['user_logged'] == None:
+        flash('Please login.', 'red')
+        # Save try in user access to redirect to here again after login
+        return redirect(url_for('login', next=url_for('edit')))
+    else:
+        game = Game.query.filter_by(id = game_id).first()
+        return render_template('edit.html', title='Edit Game',
+                                tab_name="Edit Game", game = game, cover_game = 'capa_{}.jpg'.format(game.id))
+                                
+@app.route('/update', methods=['POST',])
+def update():
+    # update values in bd, retrieve by id and get form values passing into a object
+    # or you can pass value into url and update value too (make this???)
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+    game = Game.query.get(request.form['game_id'])
+    game.name = name
+    game.category = category
+    game.console = console
+    db.session.commit()
+    return redirect(url_for('index'))
+    
+    
+@app.route('/delete/<int:game_id>', methods=['GET',])
+def delete(game_id):
+    game = Game.query.get(game_id)
+    db.session.delete(game)
+    db.session.commit()
+    flash('Game removed.', 'orange')
+    return redirect(url_for('index'))
+
+
+@app.route('/logout', methods=['GET',])
+def logout():
+    session['user_logged'] = None
+    flash('Please login again.', 'red')
+    # in url_for pass a name of method to redirect into your route
+    return redirect(url_for('login'))
+    
+    
+@app.route('/uploads/<filename>', methods=['GET',])
+def image(filename):
+    return send_from_directory('uploads', filename)
     
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -7,11 +7,6 @@ class Game(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     category = db.Column(db.String(120), unique=True, nullable=False)
     console = db.Column(db.String(120), unique=True, nullable=False)
-    
-    def __init__(self, name, category, console):
-        self.name = name
-        self.category = category
-        self.console = console
         
     def __repr__(self):
         return '<Game %r>' % self.name
@@ -20,10 +15,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=True, nullable=False)
-    
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
         
     def __repr__(self):
         return '<User %r>' % self.username
